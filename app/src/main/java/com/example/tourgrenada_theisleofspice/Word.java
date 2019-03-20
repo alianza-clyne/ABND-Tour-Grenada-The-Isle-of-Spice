b11959a03af1fa4aa0549e38132f6de92ea449bf
Alianza <octavia1011@gmail.com>
2019-03-20 15:11:58 -0400

    Removed any commented out code in Word.java.

--- app/src/main/java/com/example/tourgrenada_theisleofspice/Word.java
+++ app/src/main/java/com/example/tourgrenada_theisleofspice/Word.java
@@ -2,8 +2,8 @@ package com.example.tourgrenada_theisleofspice;
 
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
- * It contains resource IDs for the default translation, Miwok translation, audio file, and
- * optional image file for that word.
+ * It contains resource IDs for the default translation, Miwok translation, and
+ * an optional image file for that word.
  */
 public class Word {
 
@@ -12,11 +12,6 @@ public class Word {
 
     /** String resource ID for the Miwok translation of the word */
     private int mMiwokTranslationId;
-
-    /** Audio resource ID for the word */
-    /*
-    private int mAudioResourceId;
-    */
 
     /** Image resource ID for the word */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
@@ -30,19 +25,11 @@ public class Word {
      * @param defaultTranslationId is the string resource ID for the word in a language that the
      *                             user is already familiar with (such as English)
      * @param miwokTranslationId is the string resource Id for the word in the Miwok language
-     *  audioResourceId is the resource ID for the audio file associated with this word
      */
     public Word(int defaultTranslationId, int miwokTranslationId) {
         mDefaultTranslationId = defaultTranslationId;
         mMiwokTranslationId = miwokTranslationId;
     }
-    /*
-    public Word(int defaultTranslationId, int miwokTranslationId, int audioResourceId) {
-        mDefaultTranslationId = defaultTranslationId;
-        mMiwokTranslationId = miwokTranslationId;
-        mAudioResourceId = audioResourceId;
-    }
-    */
 
     /**
      * Create a new Word object words with image.
@@ -51,7 +38,6 @@ public class Word {
      *                             user is already familiar with (such as English)
      * @param miwokTranslationId is the string resource Id for the word in the Miwok language
      * @param imageResourceId is the drawable resource ID for the image associated with the word
-     * @para audioResourceId is the resource ID for the audio file associated with this word
      */
 
     public Word(int defaultTranslationId, int miwokTranslationId, int imageResourceId) {
@@ -59,16 +45,6 @@ public class Word {
         mMiwokTranslationId = miwokTranslationId;
         mImageResourceId = imageResourceId;
     }
-
-    /*
-    public Word(int defaultTranslationId, int miwokTranslationId, int imageResourceId,
-                int audioResourceId) {
-        mDefaultTranslationId = defaultTranslationId;
-        mMiwokTranslationId = miwokTranslationId;
-        mImageResourceId = imageResourceId;
-        mAudioResourceId = audioResourceId;
-    }
-    */
 
     /**
      * Get the string resource ID for the default translation of the word.
@@ -98,14 +74,5 @@ public class Word {
         return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 
-
-    /**
-     * Return the audio resource ID of the word.
-     */
-    /*
-    public int getAudioResourceId() {
-        return mAudioResourceId;
-    }
-    */
 
 }
