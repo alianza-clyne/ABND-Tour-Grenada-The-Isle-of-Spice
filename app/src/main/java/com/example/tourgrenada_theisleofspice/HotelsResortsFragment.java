a1227c7d5e855ecdcccbecc48a11d77303696d01
Alianza <octavia1011@gmail.com>
2019-03-23 13:01:11 -0400

    Added photo credit String to HotelsResortsFragment.java.

--- app/src/main/java/com/example/tourgrenada_theisleofspice/HotelsResortsFragment.java
+++ app/src/main/java/com/example/tourgrenada_theisleofspice/HotelsResortsFragment.java
@@ -29,13 +29,13 @@ public class HotelsResortsFragment extends Fragment {
         // Create a list of items
         final ArrayList<Item> items = new ArrayList<Item>();
         items.add(new Item(R.string.hotels_overview, R.string.hotels_overview_description,
-                R.drawable.color_red));
+                R.string.hotels_overview_pcredit, R.drawable.color_red));
         items.add(new Item(R.string.hotels_spice_island, R.string.hotels_spice_island_description,
-                R.drawable.color_mustard_yellow));
+                R.string.hotels_spice_island_pcredit, R.drawable.color_mustard_yellow));
         items.add(new Item(R.string.hotels_sandals, R.string.hotels_sandals_description,
-                R.drawable.color_dusty_yellow));
+                R.string.hotels_sandals_pcredit, R.drawable.color_dusty_yellow));
         items.add(new Item(R.string.hotels_calabash, R.string.hotels_calabash_description,
-                R.drawable.color_green));
+                R.string.hotels_calabash_pcredit, R.drawable.color_green));
 
         // Create an {@link ItemAdapter}, whose data source is a list of {@link Item}s. The
         // adapter knows how to create list items for each item in the list.
